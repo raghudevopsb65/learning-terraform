fbea79d18ad11f6f0336acbc55705ff2da1e0a34
Raghu K <raghuk.vit@gmail.com>
2022-07-22 06:58:48 +0530

    Update learning/learning-terraform/

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "web" {
+  // count      = 0 (We can make some condition to come with a value based on that this resource can be created)
   ami           = "ami-0eccf89e870877654"
   instance_type = var.instance_type == "null" ? "t3.micro" : var.instance_type
 
