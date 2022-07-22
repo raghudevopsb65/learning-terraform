5cd41fc61bccfe364d3904acd45cda09673dee31
Raghu K <raghuk.vit@gmail.com>
2022-07-22 06:08:28 +0530

    Update learning/learning-terraform/

---
+++ 04-loops/approach01/main.tf
@@ -0,0 +1,13 @@
+variable "no-of-instances" {
+  default = 2
+}
+
+resource "aws_instance" "web" {
+  count         = var.no-of-instances
+  ami           = "ami-0eccf89e870877654"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform"
+  }
+}
