6416999b0047f88f9b43e14c5c36e3af5264a377
Raghu K <raghuk.vit@gmail.com>
2022-07-22 06:38:29 +0530

    Update learning/learning-terraform/

--- 05-functions/main.tf
+++ 05-functions/main.tf
@@ -2,6 +2,18 @@ variable "alist" {
   default = [1, 2]
 }
 
+variable "amap" {
+  default = {
+    a = 1
+    b = 2
+  }
+}
+
+
 output "alist_length" {
   value = length(var.alist)
+}
+
+output "amap_length" {
+  value = length(var.amap)
 }
