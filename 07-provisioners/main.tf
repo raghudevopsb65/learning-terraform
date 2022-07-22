d4bfe0afa10ef88eec0340e3d0cc0e10cfd7e070
Raghu K <raghuk.vit@gmail.com>
2022-07-22 07:07:12 +0530

    Update learning/learning-terraform/

---
+++ 07-provisioners/main.tf
@@ -0,0 +1,49 @@
+resource "aws_instance" "web" {
+  ami                    = "ami-0eccf89e870877654"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "terraform"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+
+    inline = [
+      "uname"
+    ]
+  }
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-075435ce9af088bfa"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
